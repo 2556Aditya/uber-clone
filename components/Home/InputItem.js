9b45eace0e9d01fe9d63250aaa4423a538ec00bb
2556aditya <adichaurasia95@gmail.com>
2025-01-10 23:09:44 +0530

    handling API's

--- components/Home/InputItem.js
+++ components/Home/InputItem.js
@@ -1,11 +1,22 @@
+'use client';
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+
 
 function InputItem({type}) {
+  const [value, setValue] = useState(null);
   return (
     <div className='bg-slate-200 p-3 rounded-lg mt-3 flex items-center gap-4'>
         <Image src={type=='source'?'/source1.png':'/dest.png'} width={15} height={15} alt='source'/>
-        <input type='text' placeholder={type=='source'?'Pickup Location':'DropOff Location'} className='bg-transparent w-full outline-none' />
+        {/* <input type='text' placeholder={type=='source'?'Pickup Location':'DropOff Location'} className='bg-transparent w-full outline-none' />  */}
+    <GooglePlacesAutocomplete
+    apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+    selectProps={{
+      value,
+      onChange: setValue,
+    }}
+    />
     </div>
   )
 }
